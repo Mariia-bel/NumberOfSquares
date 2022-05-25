ba6df4e926e6fb2319b710e015645b05ea56fbaa
Мария <airbelka@bk.ru>
2022-05-25 16:53:02 +0300

    Rework commit

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,14 +1,17 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int squares(int rangeMin, int rangeMax, int expected) {
+    public int squares() {
         int count = 0;
+        int rangeMin = 200;
+        int rangeMax = 300;
         for (int i = 10; i <= 99; i++) {
             int x = i * i;
-            if (x >= rangeMin)
+            if (x >= rangeMin) {
                 if (x <= rangeMax) {
                     count++;
                 }
+            }
         }
         return count;
     }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,15 +7,14 @@ public class SQRServiceTest {
 
     @org.junit.jupiter.params.ParameterizedTest
     @CsvSource({
-            "200, 300, 3",
-            "-100, 100, 1",
-            "0, 99, 0"
+            "200, 300, 3"
+
 
     })
 
     public void sqtest(int rangeMin, int rangeMax, int expected) {
         SQRService service = new SQRService();
-        int actual = service.squares(rangeMin, rangeMax, expected);
+        int actual = service.squares();
 
         Assertions.assertEquals(expected, actual);
 
